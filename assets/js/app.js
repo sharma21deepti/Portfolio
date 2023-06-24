1913cffa31db7d8f79d0247f45215f9af3dee2f5
sharma21deepti <deepti21aug@gmail.com>
2023-06-24 11:09:07 +0530

    Resume

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,7 @@ let header = $(`
   
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
-   
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/images/Deepti_Sharma_resume.pdf" download>Resume</a></li>
    
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
